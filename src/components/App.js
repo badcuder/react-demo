6f92c4bb107467dfb3fd7c301e819190c0498f62
Bad Cuder <badcuder@gmail.com>
2019-03-18 00:10:01 +0700

    Add SUJU logo

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,9 @@ import '../styles/App.css';
 class App extends Component {
     render() {
         return (
-            <div>
+            <div style={{textAlign: 'center'}}>
                 <h1>My React App - BadCuder!</h1>
+                <img src="https://avatars3.githubusercontent.com/u/48653662?s=460&v=4" />
             </div>
         );
     }
